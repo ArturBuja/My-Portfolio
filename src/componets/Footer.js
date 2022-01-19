869111f7d07f132ec48adb7537f17504934d885a
Bobeer95 <artur.buja2@gmail.com>
2022-01-19 14:42:46 +0100

    styling footer

--- src/componets/Footer.js
+++ src/componets/Footer.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import ghlogo from "../assets/GitHub-Mark-Light-32px.png";
+import ldlogo from "../assets/LI-In-Bug.png";
+
+
+
 
 function Footer() {
     return (<>
         <div className="footer">
+            <a href="https://github.com/ArturBuja">
+            <div className="gh--logo">
+                <img src={ghlogo} alt="Github logo"/>
+                <p>Github</p>
+            </div></a>
 
-            <span className="gh--logo">Github logo  </span>
-            <span className="ld--logo">linkedin logo </span>
+            <a href="https://www.linkedin.com/in/artur-buja-b42693225/">
+            <div className="ld--logo">
+            <img src={ldlogo} alt="Linkedin logo"/>
+            <p>Linkedin</p>
+            </div></a>
+
 
         </div>
     </>

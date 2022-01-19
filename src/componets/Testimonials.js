bef0b36ac51d522ddd64532f232cd44e799962ab
Bobeer95 <artur.buja2@gmail.com>
2022-01-19 13:57:19 +0100

    adding custom scrollbars

--- src/componets/Testimonials.js
+++ src/componets/Testimonials.js
@@ -22,39 +22,39 @@ function Testimonials() {
 
     }
     // console.log(name)
-    const fakeRepo = [
-        {
-            id: 1,
-            name: "Pig Game",
-            text: "sample text about repo sample text about reposample text about repo sample text about repo sample text about repo sample text about repo"
-        },
-        {
-            id: 2,
-            name: "Sample Game",
-            text: "sample text about repo sample text about reposample text about repo sample text about repo sample text about repo sample text about repo"
-        },
-        {
-            id: 3,
-            name: "Sample Game",
-            text: "sample text about repo sample text about reposample text about repo sample text about repo sample text about repo sample text about repo"
-        }
-    ]
+    // const fakeRepo = [
+    //     {
+    //         id: 1,
+    //         name: "Pig Game",
+    //         text: "sample text about repo sample text about reposample text about repo sample text about repo sample text about repo sample text about repo"
+    //     },
+    //     {
+    //         id: 2,
+    //         name: "Sample Game",
+    //         text: "sample text about repo sample text about reposample text about repo sample text about repo sample text about repo sample text about repo"
+    //     },
+    //     {
+    //         id: 3,
+    //         name: "Sample Game",
+    //         text: "sample text about repo sample text about reposample text about repo sample text about repo sample text about repo sample text about repo"
+    //     }
+    // ]
     return (
         <>
-            {console.log(name)}
+            {/*{console.log(name)}*/}
             <div className="about--me--section about--me--inside">
                 <div className="about--me--header ">
                     <span className="header--text">Testimonials </span>
                     <span className="header--subtext">My github sample projects </span>
                     <div className="row repo--container">
-                        { name.slice(0,3).map((value => {
+                        { name.map((value => {
                             return (
 
                                 <div key={ value.id } className="col-lg-4 col-sm-12 col-md-6  repo--inner--container">
                                     <div className="card shadow repo--card">
                                         <span className="card--name">{ value.name }</span>
                                         <span className="card--description">{ value.description }</span>
-                                        <a href={ value.clone_url }><span className="card--link">GET SOURCE</span></a>
+                                        <a href={ value.clone_url } target="_blank"><span className="card--link">GET SOURCE</span></a>
                                     </div>
                                 </div>
                             )

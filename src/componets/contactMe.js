8ad1e6c08df88926d717c315d7f3236fe3c10e76
Bobeer95 <artur.buja2@gmail.com>
2022-01-21 16:41:10 +0100

    Adding mail

--- src/componets/contactMe.js
+++ src/componets/contactMe.js
@@ -1,9 +1,29 @@
 import React, {useState} from 'react';
+import emailjs from '@emailjs/browser';
+import {useRef} from "react";
 
 function ContactMe() {
     const [name, setName] = useState("")
+
     const [email, setEmail] = useState("")
+    const form = useRef();
     const [message, setMessage] = useState("")
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_uwh1rwk', 'template_wsm7ggl', form.current, 'user_27h1MqqBApNu181G79mlX')
+            .then((result) => {
+                console.log(result.text);
+                alert("Mail has been sent. I will contact you as soon as possible. Thanks.\n")
+            }, (error) => {
+                console.log(error.text);
+                alert("Something went wrong. Mail was not sent, please try again.\n")
+            });
+
+        setName("")
+        setEmail("")
+        setMessage("")
+    };
     return (
         <div className="contact--me" id="contactMe">
             <div className="contact--me-bg"></div>
@@ -24,23 +44,23 @@ function ContactMe() {
                             </div>
                         </div>
                         <div className="col-lg-6 col-md-5 col-sm-12 my-auto right--contact">
-                            <div className="card--contact--right">
+                            <form ref={form} onSubmit={sendEmail} className="card--contact--right">
                                 <div className="input--group">
                                     <label> Name </label>
-                                    <input onChange={(e =>{setName(e.target.value)})} value={name} className="input--groups" type="text" placeholder="Enter your name"/>
+                                    <input name="name" onChange={(e =>{setName(e.target.value)})} value={name} className="input--groups" type="text" placeholder="Enter your name"/>
                                 </div>
                                 <div className="input--group">
                                     <label> E-mail </label>
-                                    <input onChange={(e =>{setEmail(e.target.value)})} value={email} className="input--groups" type="text" placeholder="Enter your email"/>
+                                    <input name="email" onChange={(e =>{setEmail(e.target.value)})} value={email} className="input--groups" type="text" placeholder="Enter your email"/>
                                 </div>
                                 <div className="input--group">
                                     <label> Message </label>
-                                    <textarea onChange={(e =>{setMessage(e.target.value)})} value={message} className="input--groups" type="text" placeholder="Enter your message"/>
+                                    <textarea name="message" onChange={(e =>{setMessage(e.target.value)})} value={message} className="input--groups" type="text" placeholder="Enter your message"/>
                                 </div>
                                 <div className="input--group">
-                                    <button className="btn btn-success">Send message</button>
+                                    <input className="btn btn-success" value="Send message" type="submit"/>
                                 </div>
-                            </div>
+                            </form>
                         </div>
                     </div>
                 </div>
